d10399c2180f9512b7d3acb3213699ab04039a1e
IDGS-802-23002409 <unsunken09@gmail.com>
2026-02-25 20:31:43 -0600

    abc completo 25/02/26

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 from flask import g
 import forms
-
-from models import db, email
+from flask_migrate import Migrate
+from models import db
 from models import Alumnos
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
 csrf=CSRFProtect()
+db.init_app(app)
+migrate=Migrate(app,db)
 
 @app.route("/",methods=['GET', 'POST'])
 @app.route("/index")
@@ -29,8 +31,8 @@ def alumnos():
 	create_from=forms.UserForm2(request.form)
 	if request.method=='POST':
 		alum=Alumnos(nombre=create_from.nombre.data,
-			         apaterno=create_from.apaterno.data,
-					 email=create_from.email.data)
+			        apaterno=create_from.apaterno.data,
+					email=create_from.email.data)
 		db.session.add(alum)
 		db.session.commit()
 		return redirect(url_for('index'))
@@ -73,6 +75,24 @@ def modificar():
 		return redirect(url_for('index'))
 	return render_template("alumnos.html", form=create_from)
 
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eiminar():
+	create_from=forms.UserForm2(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		create_from.id.data=request.args.get('id')
+		create_from.nombre.data=alum1.nombre
+		create_from.apaterno.data=alum1.apaterno
+		create_from.email.data=alum1.email
+	if request.method=='POST':
+		id=create_from.id.data
+		alum1 = Alumnos.query.get(id)
+		db.session.delete(alum1)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("alumnos.html", form=create_from)
+
 @app.errorhandler(404)
 def page_not_fount(e):
     return render_template('404.html'), 404
@@ -80,6 +100,7 @@ def page_not_fount(e):
 if __name__ == '__main__':
 	#app.run(debug=True)
 	csrf.init_app(app)
+ 
 	with app.app_context():
 		db.create_all()
 	app.run()
